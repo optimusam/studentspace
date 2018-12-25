010b98c8464ac1b9c403436a39a8b44bd83f1a2f
Sameer Giri <sameergiri1997@gmail.com>
2018-12-25 19:49:29 +0530

    Final touches

--- import.py
+++ import.py
@@ -1,28 +1,33 @@
 from models import *
 from app import app
 import json
+import os
+import csv
+
 
 def insertCollege():
-    # db.drop_all(Review)
+    db.drop_all()
     db.create_all()
-    # college = College(name="SRM Institute of Science and Technology", description="SRM Institute of Science and Technology, or Sri Ramaswamy Memorial Institute of Science and Technology, formerly known as SRM University, is a deemed university located in Kattankulathur, Tamil Nadu, India. It was founded in 1985 as SRM Engineering College in Kattankulathur, under University of Madras.", location="Kattankulathur, Tamil Nadu")
-    # db.session.add(college)
-    # db.session.commit()
+    college = College(name="SRM Institute of Science and Technology", description="SRM Institute of Science and Technology, or Sri Ramaswamy Memorial Institute of Science and Technology, formerly known as SRM University, is a deemed university located in Kattankulathur, Tamil Nadu, India. It was founded in 1985 as SRM Engineering College in Kattankulathur, under University of Madras.", location="Kattankulathur, Tamil Nadu")
+    db.session.add(college)
+    db.session.commit()
 
 def fillData():
-    fileName = ["profIT.json", "profCSE.json"]
-    for file in fileName:
-        file = open(f"import/{file}", "r")
-        teachers_json = json.loads(file.read())["teachers"]
-
-        for teacher in teachers_json:
-            teacher = Teacher(name=teacher["name"], department=teacher["department"], designation=teacher["designation"], college_id=1)
-            db.session.add(teacher)
+    filenames = os.listdir('./import')
+    for filename in filenames:
+        with open(f"./import/{filename}") as csvFile:
+            reader = csv.reader(csvFile, delimiter=',')
+            for row in reader:
+                try:
+                    t= Teacher(name=row[0], designation=row[1],department=row[2], college_id=1)
+                    db.session.add(t)
+                except Exception as e:
+                    print("Error-->",e, t.name, t.designation, t.department)
             db.session.commit()
-
+            print("Done {filename}")
 def main():
     insertCollege()
-    # fillData()
+    fillData()
     print("DONE!")
 
 if __name__ == "__main__":
